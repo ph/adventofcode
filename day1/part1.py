3971161674035658aebe99f994b47f03aaf24c52
Pier-Hugues Pellerin <phpellerin@gmail.com>
2016-12-01 21:11:16 -0500

    Day - part 1

---
+++ day1/part1.py
@@ -0,0 +1,17 @@
+map = "L4, R2, R4, L5, L3, L1, R4, R5, R1, R3, L3, L2, L2, R5, R1, L1, L2, R2, R2, L5, R5, R5, L2, R1, R2, L2, L4, L1, R5, R2, R1, R1, L2, L3, R2, L5, L186, L5, L3, R3, L5, R4, R2, L5, R1, R4, L1, L3, R3, R1, L1, R4, R2, L1, L4, R5, L1, R50, L4, R3, R78, R4, R2, L4, R3, L4, R4, L1, R5, L4, R1, L2, R3, L2, R5, R5, L4, L1, L2, R185, L5, R2, R1, L3, R4, L5, R2, R4, L3, R4, L2, L5, R1, R2, L2, L1, L2, R2, L2, R1, L5, L3, L4, L3, L4, L2, L5, L5, R2, L3, L4, R4, R4, R5, L4, L2, R4, L5, R3, R1, L1, R3, L2, R2, R1, R5, L4, R5, L3, R2, R3, R1, R4, L4, R1, R3, L5, L1, L3, R2, R1, R4, L4, R3, L3, R3, R2, L3, L3, R4, L2, R4, L3, L4, R5, R1, L1, R5, R3, R1, R3, R4, L1, R4, R3, R1, L5, L5, L4, R4, R3, L2, R1, R5, L3, R4, R5, L4, L5, R2"
+
+movements = ((0, 1), (1, 0), (0, -1), (-1, 0))
+distance = [0, 0]
+
+d = 0
+
+for splitted in map.split(", "):
+    direction = -1 if splitted[0] == "L" else 1
+    len = int(splitted[1:])
+
+    d = (d + direction) % 4
+
+    distance[0] = distance[0] + movements[d][0] * len
+    distance[1] = distance[1] + movements[d][1] * len
+
+print abs(distance[0]) + abs(distance[1])
